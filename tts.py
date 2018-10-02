f200162d4d09c301cb804e7b96204c0b8edf0793
Eike <eikehoeppe@googlemail.com>
2018-10-02 21:30:41 +0200

    prerelease
    added arguments

--- tts.py
+++ tts.py
@@ -1,2 +1,29 @@
-#!/usr/bin/env python3
-import pyttsx3
+#!/usr/bin/python
+
+import pyttsx, sys, getopt
+
+def main(argv):
+    inputstring = ""
+    try:
+        opts, arg = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'test.py -i <inputstring> -o <outputfile>'
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'tts -i <inputstring>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputstring = arg
+            return inputstring
+
+    print 'Input file is "', inputstring
+
+if __name__ =="__main__":
+
+    input = main(sys.argv[1:])
+
+    engine = pyttsx.init()
+    engine.say(input)
+    engine.runAndWait()
